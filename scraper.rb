df60e925fb1a9b2da53b1ffbad1d3c7e1c3065df
Matthew Landauer <mlandauer@gmail.com>
2019-06-13 12:47:23 +1000

    No need to save comment_url

--- scraper.rb
+++ scraper.rb
@@ -10,14 +10,13 @@ class Mechanize::Form
   end
 end
 
-def process_page(page, base_url, comment_url)
+def process_page(page, base_url)
   page.search('tr.rgRow,tr.rgAltRow').each do |tr|
     record = {
       "council_reference" => tr.search('td')[1].inner_text.gsub("\r\n", "").strip,
       "address" => tr.search('td')[3].inner_html.gsub("\r", " ").strip.split("<br>")[0],
       "description" => tr.search('td')[3].inner_html.gsub("\r", " ").strip.split("<br>")[1],
       "info_url" => base_url + tr.search('td').at('a')["href"].to_s,
-      "comment_url" => comment_url,
       "date_scraped" => Date.today.to_s,
       "date_received" => Date.parse(tr.search('td')[2].inner_text.gsub("\r\n", "").strip).to_s,
     }
@@ -41,13 +40,12 @@ puts "Getting data in `" + period + "`, changable via MORPH_PERIOD variable"
 
 base_url = "https://planning.mackay.qld.gov.au/masterview/Modules/Applicationmaster/"
 url = base_url + "default.aspx?page=found&4a=443,444,445,446,487,555,556,557,558,559,560,564&6=F&1=" + period
-comment_url = "mailto:development.services@mackay.qld.gov.au"
 
 agent = Mechanize.new
 page = agent.get(url)
 
 if page.search('div.rgNumPart a').empty?
-  process_page(page, base_url, comment_url)
+  process_page(page, base_url)
 else
   i = 1
   page.search('div.rgNumPart a').each do |a|
@@ -55,7 +53,7 @@ else
     target, argument = a[:href].scan(/'([^']*)'/).flatten
     page = page.form.postback target, argument
 
-    process_page(page, base_url, comment_url)
+    process_page(page, base_url)
     i += 1
   end
 end
